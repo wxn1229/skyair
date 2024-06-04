4683d25ce562b856d13ae5998dd60326e9baaa55
wxn1229 <wxn1229@gmail.com>
2024-06-04 11:41:03 +0800

    create

--- airplane/src/utils/axios.ts
+++ airplane/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  //   baseURL: process.env.NEXT_PUBLIC_SERVER_URL, // 替換為你的 API 基本 URL
-  baseURL: "http://localhost:3004", // 替換為你的 API 基本 URL
+  baseURL: "https://immensely-pumped-chimp.ngrok-free.app", // 替換為你的 API 基本 URL
+  // baseURL: "http://localhost:3004", // 替換為你的 API 基本 URL
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
